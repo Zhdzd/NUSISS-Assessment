9d242a39f37e0e52fdf92e19037ab904e845c6b7
Zhdzd <zahidabdullah18@gmail.com>
2021-12-14 17:11:29 +0800

    last update

--- assessment/src/main/java/ibf/assessment/HttpServer.java
+++ assessment/src/main/java/ibf/assessment/HttpServer.java
@@ -7,62 +7,64 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.ExecutorService;
-
+import java.util.concurrent.Executors;
 
 //webserver
 public class HttpServer {
 
- 
     private int port;
-    private String []docRoot;
+    private String[] docRoot;
     private ServerSocket ss;
     private ExecutorService threadPool;
 
-
-    public HttpServer(int port, String[] docRoot){
+    public HttpServer(int port, String[] docRoot) {
         this.port = port;
         this.docRoot = docRoot;
     }
-    public String[] getDocRoot(){
+
+    public String[] getDocRoot() {
         return this.docRoot;
     }
-    
-    public void start(){
-        
-        for (int i =0;i<docRoot.length;i++ ){
+
+    public void start() {
+
+        for (int i = 0; i < docRoot.length; i++) {
             Path path = Paths.get(docRoot[i]);
-            if (!Files.isDirectory(path)){
+            if (!Files.isDirectory(path)) {
                 System.exit(1);
             }
-            if(!Files.isReadable(path)){
+            if (!Files.isReadable(path)) {
                 System.exit(1);
             }
         }
 
-        this.initializeServer();{ 
-        try {
-            this.startingServer();
-        } catch (IOException e ) {
-            System.out.println("socket having issues....");
-            System.exit(1);
-        }
-        return;
+        threadPool = Executors.newFixedThreadPool(3);
+
+        this.initializeServer();
+        {
+            try {
+                this.startingServer();
+            } catch (IOException e) {
+                System.out.println("socket having issues....");
+                System.exit(1);
+            }
+            return;
         }
     }
 
-    private void startingServer() throws IOException{
-        while(true){
+    private void startingServer() throws IOException {
+        while (true) {
             Socket socket = ss.accept();
             threadPool.submit(new HttpClientConnection(socket));
         }
-     void initializeServer() {
-         try{
-             this.ss = new ServerSocket(port);
-         } catch(IOException e){
-             System.out.println("port "+port +"having issues..starting");
-         }
+    }
 
+    void initializeServer() {
+        try {
+            this.ss = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println("port " + port + "having issues..starting");
+        }
 
-
-    } 
+    }
 }
--- assessment/src/main/java/ibf/assessment/Main.java
+++ assessment/src/main/java/ibf/assessment/Main.java
@@ -3,6 +3,7 @@ package ibf.assessment;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class Main {
     public static void main(String[] args) throws IOException{
@@ -25,11 +26,7 @@ public class Main {
        HttpServer server = new HttpServer(port, docRoot);
        server.start();
        
-
        
-      
-        
-        
     }
 
 
