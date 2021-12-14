0d1adbc22afe478e0b78f6eef35b7693932e3d7a
Zhdzd <zahidabdullah18@gmail.com>
2021-12-14 14:19:56 +0800

    update

--- assessment/src/main/java/ibf/assessment/HttpServer.java
+++ assessment/src/main/java/ibf/assessment/HttpServer.java
@@ -9,14 +9,51 @@ public class HttpServer {
 
  
     private int port;
-    private String[] docRoot;
+    private String []docRoot;
+    private ServerSocket ss;
 
 
     public HttpServer(int port, String[] docRoot){
         this.port = port;
         this.docRoot = docRoot;
     }
+    public String[] getDocRoot(){
+        return this.docRoot;
+    }
+    
+    public void start(){
+        this.initializeServer();{ 
+        try {
+            this.startingServer();
+        } catch (IOException e ) {
+            System.out.println("socket having issues....");
+            System.exit(1);
+        }
+        return;
+        }
+    }
+     void initializeServer() {
+         try{
+             this.ss = new ServerSocket(port);
+         } catch(IOException){
+             System.out.println("port "+port +"having issues..starting");
+         }
 
+    private void startingServer() throws IO Exception{
+        while(true){
+            Socket socket = ss.accept();
+        }
+
+    }
+
+
+
+    // put inside httpserver)
+
+
+    
+     
+    
     
 
     
